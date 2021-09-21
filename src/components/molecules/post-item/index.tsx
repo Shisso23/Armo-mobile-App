83a4210cccd83e64d91f628bc516351f89a24855
hyacinthe <hyacinthe@codehesion.co.za>
2021-09-21 13:38:31 +0200

    made separate component for PostItem

---
+++ src/components/molecules/post-item/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Text, Icon } from 'react-native-elements';
+import { List } from 'react-native-paper';
+
+import useTheme from '../../../theme/hooks/useTheme';
+import { Colors } from '../../../theme/Variables';
+
+type PostItemProps = {
+  item: { title: String; description: String; id: any };
+  handleJoinForum: any;
+  onSelect: any;
+};
+
+const PostItem: React.FC<PostItemProps> = ({ item, handleJoinForum, onSelect }) => {
+  const { Layout, Common, Gutters } = useTheme();
+
+  return (
+    <View style={[Common.inputWithRoundBorders, Gutters.tinyMargin, styles.forumItem]}>
+      <List.Item
+        title={item.title}
+        titleNumberOfLines={2}
+        titleStyle={Common.cardTitle}
+        description={() => (
+          <View style={Gutters.smallTMargin}>
+            <Text style={Common.cardDescription}>{item.description}</Text>
+            <TouchableOpacity
+              style={[Layout.row, Layout.alignSelfEnd, Gutters.smallTMargin]}
+              onPress={handleJoinForum}
+            >
+              <Text style={[Common.link, Gutters.tinyRMargin, styles.joinForumText]}>
+                Join Forum
+              </Text>
+              <Icon
+                name="arrow-right-circle"
+                type="feather"
+                color={Colors.secondary}
+                iconStyle={styles.iconsOpacity}
+                size={14}
+                containerStyle={styles.joinForumIcon}
+              />
+            </TouchableOpacity>
+          </View>
+        )}
+        onPress={() => {
+          onSelect(item);
+        }}
+        descriptionNumberOfLines={10}
+        descriptionStyle={{}}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  forumItem: {
+    shadowOffset: {
+      width: 2,
+      height: 3,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+  },
+  iconsOpacity: { opacity: 0.72 },
+  joinForumIcon: { marginTop: 3 },
+  joinForumText: { fontWeight: '300' },
+});
+
+export default PostItem;
--- src/screens/app/forums/forums.screen.tsx
+++ src/screens/app/forums/forums.screen.tsx
@@ -1,14 +1,7 @@
 import React, { useState, createRef } from 'react';
-import {
-  View,
-  StyleSheet,
-  FlatList,
-  RefreshControl,
-  TouchableOpacity,
-  Keyboard,
-} from 'react-native';
+import { View, StyleSheet, FlatList, RefreshControl, Keyboard } from 'react-native';
 import { Text, Icon } from 'react-native-elements';
-import { FAB, List } from 'react-native-paper';
+import { FAB } from 'react-native-paper';
 import ActionSheet from 'react-native-actions-sheet';
 import { useNavigation } from '@react-navigation/native';
 
@@ -17,6 +10,7 @@ import { useTheme } from '../../../theme';
 import { ScreenContainer } from '../../../components';
 import CategoryActionSheet from '../../../components/molecules/category-action-sheet-content';
 import SearchBar from '../../../components/atoms/search-bar';
+import PostItem from '../../../components/molecules/post-item';
 
 const ForumsScreen: React.FC = () => {
   const navigation = useNavigation();
@@ -83,41 +77,7 @@ const ForumsScreen: React.FC = () => {
   };
 
   const renderForum = ({ item }: { item: { title: String; description: String; id: any } }) => {
-    return (
-      <View style={[Common.inputWithRoundBorders, Gutters.tinyMargin, styles.forumItem]}>
-        <List.Item
-          title={item.title}
-          titleNumberOfLines={2}
-          titleStyle={Common.cardTitle}
-          description={() => (
-            <View style={Gutters.smallTMargin}>
-              <Text style={Common.cardDescription}>{item.description}</Text>
-              <TouchableOpacity
-                style={[Layout.row, Layout.alignSelfEnd, Gutters.smallTMargin]}
-                onPress={handleJoinForum}
-              >
-                <Text style={[Common.link, Gutters.tinyRMargin, styles.joinForumText]}>
-                  Join Forum
-                </Text>
-                <Icon
-                  name="arrow-right-circle"
-                  type="feather"
-                  color={Colors.secondary}
-                  iconStyle={styles.iconsOpacity}
-                  size={14}
-                  containerStyle={styles.joinForumIcon}
-                />
-              </TouchableOpacity>
-            </View>
-          )}
-          onPress={() => {
-            navigation.navigate('ViewForum', { item });
-          }}
-          descriptionNumberOfLines={10}
-          descriptionStyle={{}}
-        />
-      </View>
-    );
+    return <PostItem item={item} handleJoinForum={handleJoinForum} onSelect={() => {}} />;
   };
 
   return (
@@ -188,17 +148,7 @@ const styles = StyleSheet.create({
     right: '25%',
     transform: [{ rotate: '90deg' }],
   },
-  forumItem: {
-    shadowOffset: {
-      width: 2,
-      height: 3,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-  },
   iconsOpacity: { opacity: 0.72 },
-  joinForumIcon: { marginTop: 3 },
-  joinForumText: { fontWeight: '300' },
   title: { fontWeight: '500', marginLeft: '5%' },
 });
 
