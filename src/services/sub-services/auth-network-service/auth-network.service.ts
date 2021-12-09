f075275f5e77181e2756856bedc00c7118e19df2
hyacinthe <hyacinthe@codehesion.co.za>
2021-12-09 11:34:03 +0200

    fixed refresh token issue
    Added requests and responses logs for better debugging experience

--- src/services/sub-services/auth-network-service/auth-network.service.ts
+++ src/services/sub-services/auth-network-service/auth-network.service.ts
@@ -1,5 +1,6 @@
-import ax from 'axios';
+import ax, { AxiosResponse } from 'axios';
 import createAuthRefreshInterceptor from 'axios-auth-refresh';
+import formUrlEncoded from 'form-urlencoded';
 
 import userAuthUrls from '../user-auth-service/user-auth.urls';
 import userAuthUtils from '../user-auth-service/user-auth.utils';
@@ -14,17 +15,54 @@ const authNetworkService = ax.create({
   headers: {
     Accept: 'application/json',
     'content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
   },
   responseType: 'json',
 });
 
-const refreshTokenLogic = () => {
+if (__DEV__) {
+  authNetworkService.interceptors.request.use(
+    (requestConfig) => {
+      const { method, url, data, headers } = requestConfig;
+      console.log(`🤔 ${method?.toUpperCase()} ${url}`, { data, headers }); // eslint-disable-line no-console
+      return requestConfig;
+    },
+    (error) => {
+      console.log('❌', error); // eslint-disable-line no-console
+      return Promise.reject(error);
+    },
+  );
+  authNetworkService.interceptors.response.use(
+    (response) => {
+      const {
+        data,
+        headers,
+        config: { url, method },
+      } = response;
+      console.log(`✅ ${method?.toUpperCase()} "${url}"`, { data, headers }); // eslint-disable-line no-console
+      return response;
+    },
+    (error) => {
+      console.log('❌', error); // eslint-disable-line no-console
+      return Promise.reject(error);
+    },
+  );
+}
+
+const refreshConfig = {
+  headers: {
+    Accept: '*/*',
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+};
+
+export const refreshTokenLogic = () => {
   const _tryToRefreshToken = (refreshOAuthData: Object) => {
     const tokenUrl = userAuthUrls.tokenUrl();
-    return ax.post(tokenUrl, refreshOAuthData);
+    return ax.post(tokenUrl, formUrlEncoded(refreshOAuthData), refreshConfig);
   };
 
-  const _storeNewTokens = (apiResponse: Object) =>
+  const _storeNewTokens = (apiResponse: AxiosResponse<any>) =>
     userAuthUtils.storeAccessAndRefreshTokens(apiResponse);
 
   return Promise.resolve()
