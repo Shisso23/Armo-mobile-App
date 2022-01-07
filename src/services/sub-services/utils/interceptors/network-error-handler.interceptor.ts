b684cb17078623d1dc7e07148f7169a311b027dc
Hyacinthe Ebula <hyacinthe@codehesion.co.za>
2022-01-07 13:20:44 +0200

    - sign the user out if there's an authentication error code

--- src/services/sub-services/utils/interceptors/network-error-handler.interceptor.ts
+++ src/services/sub-services/utils/interceptors/network-error-handler.interceptor.ts
@@ -1,16 +1,21 @@
 import _ from 'lodash';
+import { AxiosError, AxiosInstance } from 'axios';
+
 import {
   ClientNetworkError,
   ServerNetworkError,
   ServerNotFoundError,
 } from '../../../../exceptions';
-import { AxiosError, AxiosInstance } from 'axios';
+import { userAuthService } from '../../..';
+import store from '../../../../reducers/store';
+import { setIsAuthenticatedAction } from '../../../../reducers/user-auth-reducer/user-auth.reducer';
 
 const createNetworkErrorHandlerInterceptor = (axiosInstance: AxiosInstance) => {
   const _serverResponded = (error: AxiosError) => error.response;
   const _noResponseFromServer = (error: AxiosError) => error.request;
   const _serverSideError = (statusCode: number) => statusCode >= 500;
   const _clientSideError = (statusCode: number) => statusCode >= 400;
+  const _authError = (statusCode: number) => statusCode === 401;
 
   return axiosInstance.interceptors.response.use(
     (response) => response,
@@ -21,7 +26,13 @@ const createNetworkErrorHandlerInterceptor = (axiosInstance: AxiosInstance) => {
         if (_serverSideError(statusCode)) {
           exception = new ServerNetworkError(statusCode, error.response.data);
         } else if (_clientSideError(statusCode)) {
-          exception = new ClientNetworkError(statusCode, error.response.data);
+          if (_authError(statusCode)) {
+            userAuthService.signOut().then(() => {
+              store.dispatch(setIsAuthenticatedAction(false));
+            });
+          } else {
+            exception = new ClientNetworkError(statusCode, error.response.data);
+          }
         }
       } else if (_noResponseFromServer(error)) {
         exception = new ServerNotFoundError('Server is probably offline');
