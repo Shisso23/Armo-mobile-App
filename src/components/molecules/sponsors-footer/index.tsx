5b229cd846e1faf9816efa83f3e00a43d8e61f41
Hyacinthe Ebula <hyacinthe@codehesion.co.za>
2022-01-18 11:33:57 +0200

    Improvements
    - Added script to prevent commiting dependencies with wildcards
    
    - removed getNotifications setInterval and only fetch notifications when theres an actual push notification
    
    - Updated the promos footer component to only fetch all promos once and randomize from the stored ones

--- src/components/molecules/sponsors-footer/index.tsx
+++ src/components/molecules/sponsors-footer/index.tsx
@@ -1,6 +1,5 @@
-import { useFocusEffect } from '@react-navigation/core';
 import _ from 'lodash';
-import React, { useCallback, useMemo } from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import { StyleSheet, View, Image, Pressable, Linking, Dimensions } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { ActivityIndicator } from 'react-native-paper';
@@ -16,11 +15,13 @@ const screenWidth = Dimensions.get('window').width;
 const SponsorsFooter = ({ categoryId }: { categoryId?: string }) => {
   const { Layout, Gutters } = useTheme();
   const { sponsors } = useSelector(sponsorsSelector);
+  const [stateSponsors, setStateSponsors] = useState<Array<sponsorTypes>>(sponsors);
+  const [randomSponsors, setRandomSponsors] = useState<Array<sponsorTypes>>([]);
   const dispatch = useDispatch();
 
-  const randomSponsors = useMemo(() => {
-    return _.shuffle(sponsors).slice(0, 3);
-  }, [sponsors]);
+  const getRandomSponsors = () => {
+    return setRandomSponsors(_.shuffle(stateSponsors).slice(0, 3));
+  };
 
   const goToPromoWebsite = (link: string) => {
     if (link.substr(0, 5) === 'http:') {
@@ -34,17 +35,30 @@ const SponsorsFooter = ({ categoryId }: { categoryId?: string }) => {
     }
   };
 
-  useFocusEffect(
-    useCallback(() => {
-      dispatch(getSponsorsAction(categoryId ? { CategoryId: categoryId } : undefined));
-      const interval = setInterval(() => {
-        dispatch(getSponsorsAction(categoryId ? { CategoryId: categoryId } : undefined));
-      }, 5000);
-      return () => {
-        clearInterval(interval);
-      };
-    }, [categoryId]),
-  );
+  const fetchSponsors = async () => {
+    const fetchedPromos: any = await dispatch(
+      getSponsorsAction(categoryId ? { CategoryId: categoryId } : undefined),
+    );
+    setStateSponsors(fetchedPromos);
+  };
+
+  useEffect(() => {
+    getRandomSponsors();
+    const interval = setInterval(() => {
+      getRandomSponsors();
+    }, 5000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [JSON.stringify(stateSponsors)]);
+
+  useLayoutEffect(() => {
+    fetchSponsors();
+  }, []);
+
+  useEffect(() => {
+    fetchSponsors();
+  }, [categoryId]);
 
   const renderDivider = () => <View style={[styles.divider]} />;
 
@@ -69,14 +83,14 @@ const SponsorsFooter = ({ categoryId }: { categoryId?: string }) => {
   };
 
   return (
-    (_.get(sponsors, 'length', 0) > 0 && (
+    (_.get(randomSponsors, 'length', 0) > 0 && (
       <View
         style={[Layout.row, styles.footer, Layout.alignItemsCenter, Layout.justifyContentCenter]}
       >
         {renderSponsor(randomSponsors[0])}
-        {sponsors.length > 1 && renderDivider()}
+        {randomSponsors.length > 1 && renderDivider()}
         {renderSponsor(randomSponsors[1])}
-        {sponsors.length > 2 && renderDivider()}
+        {randomSponsors.length > 2 && renderDivider()}
         {renderSponsor(randomSponsors[2])}
       </View>
     )) || <View />
