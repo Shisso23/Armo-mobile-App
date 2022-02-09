164a8f8d7175d994e223eb0872e7a357e1e8ea6d
Zandre Broodryk <zandre@codehesion.co.za>
2022-02-09 09:56:22 +0200

    fix form validation errors from api display - add logic to display sign up error messages from api on the form - remove password validation requirements to be in line with the new api requirements

--- src/components/forms/form-validaton-schemas/index.ts
+++ src/components/forms/form-validaton-schemas/index.ts
@@ -1,16 +1,10 @@
 import * as Yup from 'yup';
 
-const paswordExpression =
-  /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.*[!@#$%^&*])(?=.{6,})/;
-
 export const emailSchema = Yup.string().email('Invalid Email').trim().required('Email is required');
 export const passwordSchema = Yup.string().required('Password is required');
 
 export const registerPasswordSchema = Yup.string()
-  .matches(
-    paswordExpression,
-    'Password must contain at least an uppercase and lowercase letters, digit and a special character!',
-  )
+  .length(6, 'Password needs to be at least 6 characters in length.')
   .required('Password is required');
 
 export const confirmPasswordSchema = Yup.string()
